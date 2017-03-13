bfdf136d5347ea368a7c28b387a304fe617d7676
Kevin Xie <xie.kevin11@gmail.com>
2017-03-13 10:44:52 -0700

    Initial Commit

---
+++ MatrixTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MatrixTest
+//
+//  Created by Kevin Xie on 3/13/17.
+//  Copyright © 2017 Brigade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
